a99b01991a09d7f3256119470cc700c68cb5d08f
Andrew Yasinishyn <yasinishyn.a.n@gmail.com>
2015-05-15 18:03:53 +0300

    init

---
+++ marquee.rb
@@ -0,0 +1,25 @@
+# In this challenge fake marquee text is text that is shown part by part, 
+# in a scrolling-like fashion.
+
+# Some examples:
+
+# hello 2
+
+#  h
+# he
+# el
+# ll
+# lo
+# o
+
+# foobarz 3
+
+#   f
+#  fo
+# foo
+# oob
+# oba
+# bar
+# arz
+# rz
+# z
